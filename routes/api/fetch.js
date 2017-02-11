a5a0e1605a63224f16924e8ec0dced5572d07582
Fushiki Hideki <hideki@hideki.local>
2017-02-11 23:05:51 +0900

    phantomjs

--- routes/api/fetch.js
+++ routes/api/fetch.js
@@ -1,21 +1,45 @@
 var express = require('express');
-var request = require('request');
+// var request = require('request');
+var phantom = require('phantom');
+// var cheerio = require('cheerio');
 
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
   const originalUrl = req.query.originalUrl;
   const options = {};
-  options.url = originalUrl;
-  console.log(originalUrl);
-
-  request.get(options, function (error, response, body) {
-    console.log(error)
-
-    console.log('body')
-    console.log(body)
-    res.send({ body: body });
+  // options.url = originalUrl;
+  console.log('originalUrl:' + originalUrl);
+  phantom.create()
+  .then((instance) => {
+      return instance.createPage();
+  })
+  .then(function(page) {
+    console.log('page.open');
+    page.open(originalUrl)
+    .then(function(status) {
+      console.log('status:' + status)
+      page.property('content')
+      .then(function(content) {
+        console.log(content)
+        res.send({ body: content });
+      });
+    });
+  })
+  .catch(error => {
+      console.log(error);
+      phInstance.exit();
   });
+
+
+
+  // request.get(options, function (error, response, body) {
+  //   console.log(error)
+
+  //   console.log('body')
+  //   console.log(body)
+  //   res.send({ body: body });
+  // });
 });
 
 module.exports = router;
